3ef0fd655bb6386936c9cb9276a9ffbd69bf0647
Reimagine Truth <thephantomelder@gmail.com>
2024-12-11 08:22:58 +0800

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,8 @@
+// Dark Mode Toggle Logic
+const darkModeButton = document.getElementById('dark-mode-toggle');
+
+darkModeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+});
 
